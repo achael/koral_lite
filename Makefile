ac4ee82b1b28e1beec617b2b7bc4b809bdd7fcd0
Andrew Chael <achael@outlook.com>
2021-06-02 14:40:15 -0400

    messed up the Makefile

--- Makefile
+++ Makefile
@@ -1,44 +1,23 @@
 #by default parallel, use 'make SERIAL=1' for serial
 
-ifneq ($(SERIAL),1)
-CC=mpicc 
-CFLAGS=-O3 -DMPI
+LANG		= -std=cc14
+CXX		= gcc
+OPTS		= -lm -O3 -fopenmp
+CFLAGS          = -fPIC -O2 -L/home/achael/software/lib -I/home/achael/software/include
+SOURCES		= mpi.c problem.c metric.c frames.c relele.c u2p.c p2u.c magn.c physics.c opacities.c misc.c postproc.c fileop.c silo.c
+OBJECTS		= $(SOURCES:.c=.o)
 
-else
-CC=gcc
-//CC=/usr/bin/h5cc
-//CFLAGS = -fopenmp -O2 -L/usr/lib/gcc/x86_64-linux-gnu/5.4.0/include -I/usr/include/hdf5/serial  
-CFLAGS = -fopenmp -O2 -L/home/achael/software/lib -I/home/achael/software/include
 
-endif
-
-//LIBS=-lm -lgsl -lgslcblas -lsiloh5 -lfftw3 -lrt -lhdf5_serial
-LIBS=-lm -lgsl -lgslcblas -lsilo -lfftw3 -lrt 
+LIBS=-lm -lgsl -lgslcblas -lsilo -lfftw3 -lrt
 
 RM=/bin/rm
-//OBJS = mpi.o u2prad.o magn.o silo.o postproc.o fileop.o misc.o physics.o finite.o problem.o metric.o relele.o rad.o opacities.o u2p.o frames.o p2u.o nonthermal.o 
-OBJS = mpi.o problem.o finite.o metric.o frames.o relele.o u2p.o p2u.o magn.o physics.o opacities.o misc.o postproc.o fileop.o silo.o
 
+//OBJS = mpi.o problem.o metric.o frames.o relele.o u2p.o p2u.o magn.o physics.o opacities.o misc.o postproc.o fileop.o silo.o
 
-all: ko ana avg phisli thsli phiavg 
+all: cu_exec
 
-ko: ko.o $(OBJS) Makefile ko.h problem.h mnemonics.h 
-	$(CC) $(CFLAGS) -o ko ko.o $(OBJS) $(LIBS)
-
-ana: ana.o $(OBJS)  Makefile ko.h problem.h mnemonics.h 
-	$(CC) $(CFLAGS) -o ana ana.o $(OBJS) $(LIBS)
-
-avg: avg.o $(OBJS)  Makefile ko.h problem.h mnemonics.h 
-	$(CC) $(CFLAGS) -o avg avg.o $(OBJS) $(LIBS)
-
-phisli: phisli.o $(OBJS)  Makefile ko.h problem.h mnemonics.h 
-	$(CC) $(CFLAGS) -o phisli phisli.o $(OBJS) $(LIBS)
-
-thsli: thsli.o $(OBJS)  Makefile ko.h problem.h mnemonics.h 
-	$(CC) $(CFLAGS) -o thsli thsli.o $(OBJS) $(LIBS)
-
-phiavg: phiavg.o $(OBJS)  Makefile ko.h problem.h mnemonics.h 
-	$(CC) $(CFLAGS) -o phiavg phiavg.o $(OBJS) $(LIBS)
+cu_exec: $(SOURCES) finite.cu nvcc $(LIBS) -O3 -std=cc11  -fopenmp $^ -o $@
 
 clean:
-	$(RM) -f ko ana avg phiavg phisli thsli *~ *.o *.oo
+	$(RM) -f *.o  cu_exec
+
--- finite.cu
+++ finite.cu
@@ -376,6 +376,189 @@ calc_wavespeeds()
 }
 
 
+int calc_update(ldouble dtin)
+{
+
+  int ii,ix,iy,iz,iv;
+  //#pragma omp barrier  
+  //#pragma omp parallel for private(ii,ix,iy,iz,iv) schedule (static)
+  for(ii=0;ii<Nloop_0;ii++) //domain 
+  {
+    
+      ix=loop_0[ii][0];
+      iy=loop_0[ii][1];
+      iz=loop_0[ii][2]; 
+
+      // Source term
+      ldouble ms[NV],gs[NV],val,du;
+
+      if(is_cell_active(ix,iy,iz)==0)
+      {
+        // Source terms applied only for active cells	
+	PLOOP(iv) ms[iv]=0.; 
+      }
+      else
+      {
+        // Get metric source terms ms[iv] and any other source terms gs[iv], and save combined source terms in ms[iv]
+	f_metric_source_term(ix,iy,iz,ms);
+	f_general_source_term(ix,iy,iz,gs);
+
+	PLOOP(iv) ms[iv]+=gs[iv];
+      }
+      
+      // Get the cell size in the three directions
+      ldouble dx=get_size_x(ix,0);
+      ldouble dy=get_size_x(iy,1);
+      ldouble dz=get_size_x(iz,2);
+
+      int doxl,doxr,doyl,doyr,dozl,dozr;
+      doxl=doxr=doyl=doyr=dozl=dozr=1;
+
+      //timestep
+      dt=dtin;  // dtin is an input parameter to op_explicit
+      
+      //update all conserved according to fluxes and source terms
+      for(iv=0;iv<NV;iv++)
+      {
+	ldouble flxr,flyr,flzr,flxl,flyl,flzl;
+	  
+        // Get the fluxes on the six faces.
+	// Recall that flbx, flby, flbz are the fluxes at the left walls of cell ix, iy, iz.
+	// To get the right fluxes, we need flbx(ix+1,iy,iz), etc.
+	flxl=get_ub(flbx,iv,ix,iy,iz,0);
+	flxr=get_ub(flbx,iv,ix+1,iy,iz,0);
+	flyl=get_ub(flby,iv,ix,iy,iz,1);
+	flyr=get_ub(flby,iv,ix,iy+1,iz,1);
+	flzl=get_ub(flbz,iv,ix,iy,iz,2);
+	flzr=get_ub(flbz,iv,ix,iy,iz+1,2);
+		  
+	// Compute Delta U from the six fluxes
+	du = -(flxr-flxl)*dt/dx - (flyr-flyl)*dt/dy - (flzr-flzl)*dt/dz;
+
+	// Compute new conserved by adding Delta U and the source term
+	val = get_u(u,iv,ix,iy,iz) + du + ms[iv]*dt;
+
+	// Save the new conserved to memory
+#ifdef SKIPHDEVOLUTION
+	if(iv>=NVMHD)
+#endif
+#ifdef RADIATION
+#ifdef SKIPRADEVOLUTION
+#ifdef EVOLVEPHOTONNUMBER
+	if(iv!=EE && iv!=FX && iv!=FY && iv!=FZ && iv!=NF)
+#else
+	if(iv!=EE && iv!=FX && iv!=FY && iv!=FZ)
+#endif
+#endif  // SKIPRADEVOLUTION
+#endif  // RADIATION
+#ifdef SKIPHDBUTENERGY
+	if(iv>=NVMHD || iv==UU)
+#endif
+	  set_u(u,iv,ix,iy,iz,val);	 
+
+      }  // for(iv=0;iv<NV;iv++)
+
+  }  // for(ii=0;ii<Nloop_0;ii++)
+}
+
+__global__ void calc_update_gpu_kernel(ldouble dtin, int Nloop_0)
+{
+
+  int ii,ix,iy,iz,iv;
+  ii = blockIdx.x * blockDim.x + threadIdx.x;
+  if(ii >= Nloop_0) return;
+  
+  //ix=loop_0[ii][0];
+  //iy=loop_0[ii][1];
+  //iz=loop_0[ii][2]; 
+
+      // Source term
+      ldouble ms[NV],gs[NV],val,du;
+      /*
+      if(is_cell_active(ix,iy,iz)==0)
+      {
+        // Source terms applied only for active cells	
+	for(iv=0;iv<NV;iv++) ms[iv]=0.; 
+      }
+      else
+      {
+        // Get metric source terms ms[iv] and any other source terms gs[iv], and save combined source terms in ms[iv]
+	//f_metric_source_term(ix,iy,iz,ms);
+	//f_general_source_term(ix,iy,iz,gs);
+
+        for(iv=0;iv<NV;iv++) ms[iv]+=gs[iv];
+      }
+      */
+      
+      // Get the cell size in the three directions
+      //ldouble dx=get_size_x(ix,0);
+      //ldouble dy=get_size_x(iy,1);
+      //ldouble dz=get_size_x(iz,2);
+
+      //int doxl,doxr,doyl,doyr,dozl,dozr;
+      //doxl=doxr=doyl=doyr=dozl=dozr=1;
+
+      //timestep
+      //dt=dtin;  // dtin is an input parameter to op_explicit
+      
+      //update all conserved according to fluxes and source terms
+      for(iv=0;iv<NV;iv++)
+      {
+	/*
+	ldouble flxr,flyr,flzr,flxl,flyl,flzl;
+	  
+        // Get the fluxes on the six faces.
+	// Recall that flbx, flby, flbz are the fluxes at the left walls of cell ix, iy, iz.
+	// To get the right fluxes, we need flbx(ix+1,iy,iz), etc.
+	flxl=get_ub(flbx,iv,ix,iy,iz,0);
+	flxr=get_ub(flbx,iv,ix+1,iy,iz,0);
+	flyl=get_ub(flby,iv,ix,iy,iz,1);
+	flyr=get_ub(flby,iv,ix,iy+1,iz,1);
+	flzl=get_ub(flbz,iv,ix,iy,iz,2);
+	flzr=get_ub(flbz,iv,ix,iy,iz+1,2);
+		  
+	// Compute Delta U from the six fluxes
+	du = -(flxr-flxl)*dt/dx - (flyr-flyl)*dt/dy - (flzr-flzl)*dt/dz;
+
+	// Compute new conserved by adding Delta U and the source term
+	val = get_u(u,iv,ix,iy,iz) + du + ms[iv]*dt;
+	*/
+	
+	// Save the new conserved to memory
+	/*
+#ifdef SKIPHDEVOLUTION
+	if(iv>=NVMHD)
+#endif
+#ifdef RADIATION
+#ifdef SKIPRADEVOLUTION
+#ifdef EVOLVEPHOTONNUMBER
+	if(iv!=EE && iv!=FX && iv!=FY && iv!=FZ && iv!=NF)
+#else
+	if(iv!=EE && iv!=FX && iv!=FY && iv!=FZ)
+#endif
+#endif  // SKIPRADEVOLUTION
+#endif  // RADIATION
+#ifdef SKIPHDBUTENERGY
+	if(iv>=NVMHD || iv==UU)
+#endif
+	*/
+	
+	//set_u(u,iv,ix,iy,iz,val);	 
+
+      }  // for(iv=0;iv<NV;iv++)
+
+
+}
+
+int calc_update_gpu(ldouble dtin)
+{
+
+  int TB_SIZE = 64;
+  int threadblocks = (Nloop_0 / TB_SIZE) + ((Nloop_0 % TB_SIZE)? 1:0);
+  calc_update_gpu_kernel<<<threadblocks, TB_SIZE>>>(dtin, Nloop_0);
+ 
+}
+
 //**********************************************************************
 /*! \fn int save_wavespeeds(int ix, int iy, int iz, ldouble *aaa)
  \brief saves characteristic wavespeeds from aaa[] to the global arrays. Also compute timesteps. 
@@ -1171,7 +1354,9 @@ op_explicit(ldouble t, ldouble dtin)
 
   //**********************************************************************
   // Evolve the conserved quantities
-  
+
+  calc_update(dtin);
+  /*  
 #pragma omp barrier  
 #pragma omp parallel for private(ii,ix,iy,iz,iv) schedule (static)
   for(ii=0;ii<Nloop_0;ii++) //domain 
@@ -1250,7 +1435,7 @@ op_explicit(ldouble t, ldouble dtin)
       }  // for(iv=0;iv<NV;iv++)
 
   }  // for(ii=0;ii<Nloop_0;ii++)
-
+  */
 
    /************************************************************************/
    /********* explicit *** RADIATION COUPLING  *****************************/
