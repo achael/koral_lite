c477c49afe4e039f951c0aaa7ceeae0fb3dca683
Andrew Chael <achael@princeton.edu>
2021-06-08 00:36:12 -0400

    added gpu versions of get_xb and get_size_x

--- finitegpu.cu
+++ finitegpu.cu
@@ -4,60 +4,93 @@ extern "C" {
 
 }
 
+// get grid location on boundary indexed ic in dimension idim
+// copied from get_xb macro in ko.h
+__global__ ldouble get_xb_gpu_kernel(ldouble* xb_arr, int ic, int idim)
+{
+  ldouble xb_out;
+  xb_out = (idim==0 ? xb_arr[ic+NG] :		     \
+           (idim==1 ? xb_arr[ic+NG + NX+2*NG + 1] :  \
+	   (idim==2 ? xb_arr[ic+NG + NX+2*NG +1 + NY+2*NG +1 ] : 0.)));
+
+  return xb_out;
+}
+
+// get size of cell indexed ic in dimension idim
+// copied from get_size_x in finite.c
+__global__ ldouble get_size_x_gpu_kernel(int ic, int idim)
+{
+  ldouble dx;
+  dx = get_xb_gpu_kernel(xb_arr, ic+1,idim) - get_xb_gpu_kernel(xb_arr, ic, idim);
+  return dx;
+}
+
+
 __global__ void calc_update_gpu_kernel(ldouble dtin, int Nloop_0, int* d_array, 
-  int* loop_0_0,int* loop_0_1,int* loop_0_2)
+                                       int* loop_0_ix, int* loop_0_iy, int* loop_0_iz,
+				       ldouble* xb_arr)
 {
 
   int ii;
-  ii = blockIdx.x * blockDim.x + threadIdx.x;
-  // if(ii<100)
-  //   printf("*");
-  
-  atomicAdd(d_array, 1);
-  
-    if(ii >= Nloop_0) return;
-  
-  
   int ix,iy,iz,iv;
-  ix=loop_0_0[ii];
-  iy=loop_0_1[ii];
-  iz=loop_0_2[ii]; 
+  ldouble dx,dy,dz;
+  ldouble val,du;
+  ldouble ms[NV];
+  //ldouble gs[NV]; //NOTE gs[NV] is for artifical sources, rarely used
 
-  if(ii<100){
+  // get index for this thread
+  ii = blockIdx.x * blockDim.x + threadIdx.x;
+  if(ii >= Nloop_0) return;
+  
+  atomicAdd(d_array, 1); // NOTE TODO: placeholder test
+  
+  // get indices from 1D arrays
+  ix=loop_0_ix[ii];
+  iy=loop_0_iy[ii];
+  iz=loop_0_iz[ii]; 
+
+  if(ii==22222){
     printf("D   : %d %d %d %d\n",ii, ix,iy,iz);
   }
 
   // Source term
-  //ldouble ms[NV],gs[NV],val,du;
-      
-  /*
-  if(is_cell_active(ix,iy,iz)==0)
+  // check if cell is active
+  // NOTE: is_cell_active always returns 1 -- a placeholder function put in long ago
+  
+  if(0) //if(is_cell_active(ix,iy,iz)==0)
   {
     // Source terms applied only for active cells	  
     for(iv=0;iv<NV;iv++) ms[iv]=0.; 
   }
   else
   {
-     // Get metric source terms ms[iv] and any other source terms gs[iv], and save combined source terms in ms[iv]
-     f_metric_source_term(ix,iy,iz,ms);
-     f_general_source_term(ix,iy,iz,gs);
-     for(iv=0;iv<NV;iv++) ms[iv]+=gs[iv];
+     // Get metric source terms ms[iv]
+     // and any other source terms gs[iv] 
+
+     //f_metric_source_term(ix,iy,iz,ms);  //TODO: somewhat complicated
+     //f_general_source_term(ix,iy,iz,gs); //NOTE: *very* rarely used,ignore for now
+     for(iv=0;iv<NV;iv++)
+     {
+       ms[iv] = 0; // TODO: placeholder metric term of 0
+       //ms[iv]+=gs[iv];
+     }
   }
-  /*    
+
+  
       
   // Get the cell size in the three directions
-  ldouble dx=get_size_x(ix,0);
-  ldouble dy=get_size_x(iy,1);
-  ldouble dz=get_size_x(iz,2);
+  dx = get_size_x_gpu_kernel(xb_arr,ix,0); //dx=get_size_x(ix,0);
+  dy = get_size_x_gpu_kernel(xb_arr,iy,1); //dy=get_size_x(iy,1);
+  dz = get_size_x_gpu_kernel(xb_arr,iz,2); //dz=get_size_x(iz,2);
 
-  int doxl,doxr,doyl,doyr,dozl,dozr;
-  doxl=doxr=doyl=doyr=dozl=dozr=1;
-
-  //timestep
-  //dt=dtin;  //TODO global?  // dtin is an input parameter to op_explicit
-      
-  //update all conserved according to fluxes and source terms
-      
+  if(ii==0)
+  {
+    printf("D size_x 0 %e \n", get_size_x_gpu_kernel(xb_arr,11,0));
+    printf("D size_x 1 %e \n", get_size_x_gpu_kernel(xb_arr,13,0));
+    printf("D size_x 2 %e \n", get_size_x_gpu_kernel(xb_arr5,0));
+     
+  /*
+  //update all conserved according to fluxes and source terms      
   for(iv=0;iv<NV;iv++)
   {	
 	ldouble flxr,flyr,flzr,flxl,flyl,flzl;
@@ -108,48 +141,66 @@ __global__ void calc_update_gpu_kernel(ldouble dtin, int Nloop_0, int* d_array,
 int calc_update_gpu(ldouble dtin)
 {
 
-
   int TB_SIZE = 64;   
   int *d_temp, h_temp=0;
-  int *d_loop0_0,*d_loop0_1,*d_loop0_2;
-  int *h_loop0_0,*h_loop0_1,*h_loop0_2;
-
+  int *d_loop0_ix,*d_loop0_iy,*d_loop0_iz;
+  int *h_loop0_ix,*h_loop0_iy,*h_loop0_iz;
+  ldouble *d_xb_arr;
+  
   cudaError_t err = cudaSuccess;
+
+  // Allocate device arrays 
+  
   // printf("ERROR (error code %s)!\n", cudaGetErrorString(err));
   err = cudaMalloc(&d_temp, sizeof(int));
 
-  err = cudaMalloc(&d_loop0_0, sizeof(int)*Nloop_0);
-  err = cudaMalloc(&d_loop0_1, sizeof(int)*Nloop_0);
-  err = cudaMalloc(&d_loop0_2, sizeof(int)*Nloop_0);
+  err = cudaMalloc(&d_loop0_ix, sizeof(int)*Nloop_0);
+  err = cudaMalloc(&d_loop0_iy, sizeof(int)*Nloop_0);
+  err = cudaMalloc(&d_loop0_iz, sizeof(int)*Nloop_0);
 
-  h_loop0_0 = (int*)malloc(sizeof(int)*Nloop_0);
-  h_loop0_1 = (int*)malloc(sizeof(int)*Nloop_0);
-  h_loop0_2 = (int*)malloc(sizeof(int)*Nloop_0);
+  err = cudaMalloc(&d_xb_arr, sizeof(ldouble)*(NX+1+NY+1+NZ+1+6*NG));
+  
+  // Copy data to device arrays
+  
+  // NOTE: when we add more functions to device, most of these should only be copied once
+  // Make 1D arrays of ix,iy,iz indicies and copy to device
+  h_loop0_ix = (int*)malloc(sizeof(int)*Nloop_0);
+  h_loop0_iy = (int*)malloc(sizeof(int)*Nloop_0);
+  h_loop0_iz = (int*)malloc(sizeof(int)*Nloop_0);
 
   for(int ii=0; ii<Nloop_0; ii++){
-    h_loop0_0[ii] = loop_0[ii][0];     
-    h_loop0_1[ii] = loop_0[ii][1];     
-    h_loop0_2[ii] = loop_0[ii][2];
-    if (ii<10)
-      printf("H   :  %d %d %d %d\n",ii,h_loop0_0[ii],h_loop0_1[ii],h_loop0_2[ii]) ;
+    h_loop0_ix[ii] = loop_0[ii][0];     
+    h_loop0_iy[ii] = loop_0[ii][1];     
+    h_loop0_iz[ii] = loop_0[ii][2];
+    if (ii==22222) printf("H   :  %d %d %d %d\n",ii,h_loop0_0[ii],h_loop0_1[ii],h_loop0_2[ii]) ;
   }
 
-  err =  cudaMemcpy(d_loop0_0, h_loop0_0, sizeof(int)*Nloop_0, cudaMemcpyHostToDevice);
-  err =  cudaMemcpy(d_loop0_1, h_loop0_1, sizeof(int)*Nloop_0, cudaMemcpyHostToDevice);
-  err =  cudaMemcpy(d_loop0_2, h_loop0_2, sizeof(int)*Nloop_0, cudaMemcpyHostToDevice);
+  err =  cudaMemcpy(d_loop0_ix, h_loop0_ix, sizeof(int)*Nloop_0, cudaMemcpyHostToDevice);
+  err =  cudaMemcpy(d_loop0_iy, h_loop0_iy, sizeof(int)*Nloop_0, cudaMemcpyHostToDevice);
+  err =  cudaMemcpy(d_loop0_iz, h_loop0_iz, sizeof(int)*Nloop_0, cudaMemcpyHostToDevice);
 
+  free(h_loop0_ix);
+  free(h_loop0_iy);
+  free(h_loop0_iz);
 
-  free(h_loop0_0);
-  free(h_loop0_1);
-  free(h_loop0_2);
+  // copy grid boundary data xb (global array) to device
+  // NOTE: size of xb is copied from initial malloc in misc.c 
+  printf("H size_x 0 %e \n", get_size_x(11,0));
+  printf("H size_x 1 %e \n", get_size_x(13,0));
+  printf("H size_x 2 %e \n", get_size_x(5,0));
+  
+  err =  cudaMemcpy(d_xb_arr, xb, sizeof(ldouble)*(NX+1+NY+1+NZ+1+6*NG), cudaMemcpyHostToDevice);
+  
+  // Launch calc_update_gpu_kernel
 
   int threadblocks = (Nloop_0 / TB_SIZE) + ((Nloop_0 % TB_SIZE)? 1:0);
-  printf("Test %d\n",threadblocks);fflush(stdout);
+  printf("Test %d\n", threadblocks); fflush(stdout);
 
   err = cudaMemset(d_temp,0,sizeof(int));
   // printf("ERRORMEMESET (error code %s)!\n", cudaGetErrorString(err));
 
-  calc_update_gpu_kernel<<<threadblocks, TB_SIZE>>>(dtin, Nloop_0,d_temp,d_loop0_0,d_loop0_1,d_loop0_2);
+  calc_update_gpu_kernel<<<threadblocks, TB_SIZE>>>(dtin, Nloop_0, d_temp,
+						    d_loop0_ix, d_loop0_iy, d_loop0_iz);
   err = cudaPeekAtLastError();
   cudaDeviceSynchronize();
   // printf("ERROR-Kernel (error code %s)!\n", cudaGetErrorString(err));
@@ -157,14 +208,18 @@ int calc_update_gpu(ldouble dtin)
   err =  cudaMemcpy(&h_temp, d_temp, sizeof(int), cudaMemcpyDeviceToHost);
   // printf("ERROR-Memcpy (error code %s)!\n", cudaGetErrorString(err));
 
-  printf("back from device %d\n",h_temp);
+  printf("back from device %d\n\n",h_temp);
 
-  cudaFree(d_loop0_0);
-  cudaFree(d_loop0_1);
-  cudaFree(d_loop0_2);
-
+  // Free Device Memory
+  cudaFree(d_loop0_ix);
+  cudaFree(d_loop0_iy);
+  cudaFree(d_loop0_iz);
+  cudaFree(d_xb_arr);
+  
   cudaFree(d_temp);
-  printf("\n");
+
+  // set global timestep dt
+  dt = dtin;
 
   return 0;
 }
