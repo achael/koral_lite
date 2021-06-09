549952e0bfce4c96dd72af1e7a6c0aa6784ed6d9
Andrew Chael <achael@princeton.edu>
2021-06-09 15:30:01 -0400

    copied p output from gpu to cpu before print

--- u2pgpu.cu
+++ u2pgpu.cu
@@ -1080,12 +1080,15 @@ int calc_u2p_gpu(int setflags)
   cudaEventElapsedTime(&tms, start,stop);
   printf("gpu u2p time: %0.2f \n",tms);
 
-
+  ldouble* p_tmp;
+  if((p_tmp=(ldouble*)malloc(PrimSize))==NULL) my_err("malloc err.\n");
+  err = cudaMemcpy(&p_tmp, d_p_arr, sizeof(ldouble)*Nprim, cudaMemcpyDeviceToHost);
   printf("gpu u2p pp[NV]: ");
   for(int iv=0;iv<NV;iv++)
-    printf("%e ", get_u(d_p_arr, iv, ixTEST, iyTEST, izTEST));
+    printf("%e ", get_u(p_tmp, iv, ixTEST, iyTEST, izTEST));
   printf("\n");
-	 
+  free(p_tmp);
+    
   // TODO Copy updated p back from device to global array p?
   //err = cudaMemcpy(&p, d_u_arr, sizeof(ldouble)*Nprim, cudaMemcpyDeviceToHost);
   
